1b469e8248d1ba29d3476ef417e08842b1dcd388
Cătălin Pleșu <catalinhimself@gmail.com>
2021-08-31 17:25:00 +0300

    qtile theme fix

--- qtile/config.py
+++ qtile/config.py
@@ -3,7 +3,7 @@ from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from gruvbox.gruvbox import *
-from gruvbox.dark import *
+from theme import *
 
 mod = "mod4"
 

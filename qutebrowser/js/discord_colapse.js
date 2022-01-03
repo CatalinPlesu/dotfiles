8ea7cf4658ac6071bf0cbd2e423c222e0cf8b530
Cătălin Pleșu <catalinhimself@gmail.com>
2022-01-03 13:43:58 +0200

    discord script working nice 😊

--- qutebrowser/js/discord_colapse.js
+++ qutebrowser/js/discord_colapse.js
@@ -1,18 +1,40 @@
 // discord script to collapse bars but now wont make the changell view bigger :(
 (()=>{
 function swap(v) {
-switch(v) {
-case "collapse":
-return "visible"
-default:
-return "collapse"
+    switch(v) {
+        case "collapse":
+        return "visible"
+        default:
+        return "collapse"
+    }
 }
+
+function chat_fullscreen(sidebar, chat, members) {
+    console.log(chat)
+    switch(sidebar) {
+        case "collapse": 
+            members.style.position = "absolute";
+            chat.style.position = "absolute";
+            chat.style.top = "0px";
+            chat.style.bottom = "0px";
+            chat.style.left = "0px";
+            chat.style.right = "0px";
+        break;
+        default: 
+            chat.style.position = "relative";
+            members.style.position = "relative";
+    }
 }
+
 var sidebar = document.getElementsByClassName("sidebar-2K8pFh")[0];
 sidebar.style.visibility=swap(sidebar.style.visibility);
 
-var idk_what = document.getElementsByClassName("membersWrap-2h-GB4 hiddenMembers-2dcs_q")[0];
-idk_what.style.visibility=swap(document.getElementsByClassName("membersWrap-2h-GB4 hiddenMembers-2dcs_q")[0].style.visibility);
+var memeber_list = document.getElementsByClassName("membersWrap-2h-GB4 hiddenMembers-2dcs_q")[0];
+if (memeber_list)
+memeber_list.style.visibility=swap(memeber_list.style.visibility);
+
+var chat = document.getElementsByClassName("chat-3bRxxu")[0]; 
+chat = chat_fullscreen(sidebar.style.visibility, chat, memeber_list);
 
 })()
 

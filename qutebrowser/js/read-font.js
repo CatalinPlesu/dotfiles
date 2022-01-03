f589733ee0189a8186166f67c21029a569794a1f
Cătălin Pleșu <catalinhimself@gmail.com>
2022-01-03 20:19:53 +0200

    works better

--- qutebrowser/js/read-font.js
+++ qutebrowser/js/read-font.js
@@ -1,30 +1,30 @@
-var cool_font = "fira";
-var not_cool_font = "not " + cool_font;
-var font = font || not_cool_font;
+if (!style){
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '.font { font-family: Fira Code Medium; }';
+    document.getElementsByTagName('head')[0].appendChild(style);
+}
 
-function changeFont(element){ if (element.getAttribute("style"))
-    element.setAttribute("style",element.getAttribute("style")+";font-family: Fira Code Medium");
+var font = font || "default";
+
+function changeFont(element){
+    element.classList.add("font");
     for(var i=0; i < element.children.length; i++){
         changeFont(element.children[i]);
     }
 }
 
-function changeFontBack(element){
-    if (element.getAttribute("style"))
-    element.style.fontFamily = "";
+function resetFont(element){
+    element.classList.remove("font");
     for(var i=0; i < element.children.length; i++){
-        changeFontBack(element.children[i]);
+        resetFont(element.children[i]);
     }
 }
 
-switch(font) {
-  case cool_font:
-    font = not_cool_font;
-    changeFontBack(document.getElementsByTagName("body")[0]);
-    break;
-  case not_cool_font:
-    font = cool_font;
+if (font == "default") {
     changeFont(document.getElementsByTagName("body")[0]);
-    break;
-  default:
+    font = "changed";
+} else {
+    resetFont(document.getElementsByTagName("body")[0]);
+    font = "default";
 }

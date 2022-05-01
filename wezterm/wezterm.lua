219abde4fd732039485ff77910e34550b3cd04f8
Cătălin Pleșu <catalinhimself@gmail.com>
2022-05-01 11:33:19 +0300

    final wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,4 +5,8 @@ return {
     font = wezterm.font("Fira Code"),
     font_size = 12.0,
     enable_tab_bar = false,
+    window_padding = { left = 0, right = 0, top = 0, bottom = 0, },
+    skip_close_confirmation_for_processes_named = {
+        "bash", "sh", "zsh", "fish", "tmux", 
+    },
 }

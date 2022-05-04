6189f6b77ce02e5bc323b661a06c929ea47da46d
Cătălin Pleșu <catalinhimself@gmail.com>
2022-05-04 13:31:25 +0300

    warpd config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,5 @@ return {
     font_size = 12.0,
     enable_tab_bar = false,
     window_padding = { left = 0, right = 0, top = 0, bottom = 0, },
-    skip_close_confirmation_for_processes_named = {
-        "bash", "sh", "zsh", "fish", "tmux", 
-    },
+    window_close_confirmation = "NeverPrompt",
 }

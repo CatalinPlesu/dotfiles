7be1638df83a74a50d270dd8833875db94b1033e
Catalin Plesu <catalinhimself@gmail.com>
2025-09-04 20:07:27 +0300

    restore qtile

---
+++ qtile/gruvbox/light.py
@@ -0,0 +1,16 @@
+#light
+from gruvbox.gruvbox import *
+### main colors
+background=white0
+foreground=black
+### borders
+focus_t=green
+normal_t=blue
+focus_f=aqua
+normal_f=white
+### bar
+mark=green
+active=foreground
+inactive=background
+###
+warning=red0

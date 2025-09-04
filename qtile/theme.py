43811903fc4eb42673d6632e05bc6f595f23fcab
Catalin Plesu <catalinhimself@gmail.com>
2025-09-04 20:07:27 +0300

    restore qtile

---
+++ qtile/theme.py
@@ -0,0 +1,16 @@
+#dark
+from gruvbox.gruvbox import *
+### main colors
+background=black
+foreground=white0
+### borders
+focus_t=green
+normal_t=blue
+focus_f=aqua
+normal_f=white
+### bar
+mark=green
+active=foreground
+inactive=background
+###
+warning=red0

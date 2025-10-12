fbacf220833954620f03aa2d1bd77b3da2061f68
Catalin Plesu <catalinhimself@gmail.com>
2025-10-12 11:41:15 +0300

    fix: syntax highlight using treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -984,23 +984,29 @@ require("lazy").setup({
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		-- main = "nvim-treesitter.configs", -- Sets main module to use for opts
+		main = "nvim-treesitter.configs", -- Sets main module to use for opts
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 		opts = {
-			config = function()
-				require("nvim-treesitter.configs").setup({
-					ensure_installed = { "lua", "vim", "c", "python" }, -- Add languages you need
-					highlight = { enable = true },
-					indent = { enable = true },
-				})
-			end,
+			ensure_installed = {
+				"bash",
+				"c",
+				"diff",
+				"html",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"vim",
+				"vimdoc",
+				"heex",
+				"elixir",
+			},
+			-- Autoinstall languages that are not installed
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
 		},
-		-- There are additional nvim-treesitter modules that you can use to interact
-		-- with nvim-treesitter. You should go explore a few and see what interests you:
-		--
-		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 	},
 
 	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the

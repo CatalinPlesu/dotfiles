05869bba8004a129ddc90a82cfff78d84665a225
Catalin Plesu <catalinhimself@gmail.com>
2025-12-02 22:03:00 +0200

    fix: remove dplicated LuaSnip declaration

--- nvim/init.lua
+++ nvim/init.lua
@@ -568,26 +568,20 @@ require("lazy").setup({
 				"L3MON4D3/LuaSnip",
 				version = "2.*",
 				build = (function()
-					-- Build Step is needed for regex support in snippets.
-					-- This step is not supported in many windows environments.
-					-- Remove the below condition to re-enable on windows.
 					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
 						return
 					end
 					return "make install_jsregexp"
 				end)(),
 				dependencies = {
-					-- `friendly-snippets` contains a variety of premade snippets.
-					--    See the README about individual language/framework/plugin snippets:
-					--    https://github.com/rafamadriz/friendly-snippets
-					-- {
-					--   'rafamadriz/friendly-snippets',
-					--   config = function()
-					--     require('luasnip.loaders.from_vscode').lazy_load()
-					--   end,
-					-- },
+					"rafamadriz/friendly-snippets",
 				},
-				opts = {},
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+					require("luasnip.loaders.from_lua").lazy_load({
+						paths = vim.fn.stdpath("config") .. "/snippets/",
+					})
+				end,
 			},
 			"folke/lazydev.nvim",
 		},
@@ -731,19 +725,6 @@ require("lazy").setup({
 
 			-- ... and there is more!
 			--  Check out: https://github.com/echasnovski/mini.nvim
-		end,
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		version = "v2.*",
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-		},
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
-			require("luasnip.loaders.from_lua").lazy_load({
-				paths = vim.fn.stdpath("config") .. "/snippets/",
-			})
 		end,
 	},
 	{

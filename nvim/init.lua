bcdd86a6a1793056fae8b0d2fb3bbf7ff7e1f459
Catalin Plesu <catalin.plesu@jivygroup.com>
2026-01-14 17:47:00 +0200

    remove auto pairs

--- nvim/init.lua
+++ nvim/init.lua
@@ -298,7 +298,6 @@ require("lazy").setup({
 			require("mini.surround").setup()
 			require("mini.files").setup()
 			require("mini.comment").setup()
-			require("mini.pairs").setup()
 			require("mini.bufremove").setup()
 		end,
 	},

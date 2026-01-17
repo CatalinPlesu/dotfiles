50d000d4e7c35e2f4b6a9bdc91ab5c6e56e8e8a5
Catalin Plesu <catalinhimself@gmail.com>
2026-01-17 17:08:02 +0200

    feat: easy dotnet

--- nvim/init.lua
+++ nvim/init.lua
@@ -625,6 +625,14 @@ require("lazy").setup({
 	},
 
 	{
+		"GustavEikaas/easy-dotnet.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+		config = function()
+			require("easy-dotnet").setup()
+		end,
+	},
+
+	{
 		"neovim/nvim-lspconfig",
 		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {

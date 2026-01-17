626c4b606513267fd281f6a8a1583f8e60c0a17d
Catalin Plesu <catalinhimself@gmail.com>
2026-01-17 16:14:38 +0200

    feat: paste and preview images

--- nvim/init.lua
+++ nvim/init.lua
@@ -887,6 +887,27 @@ require("lazy").setup({
 			{ "<leader>ws", "<cmd>lua require('neowiki').open_wiki()<cr>", desc = "Open Wiki" },
 		},
 	},
+	{
+		"HakonHarnes/img-clip.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add options here
+			-- or leave it empty to use the default settings
+		},
+		keys = {
+			-- suggested keymap
+			{ "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+		},
+	},
+	{
+		"3rd/image.nvim",
+		build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+		opts = {
+			backend = "kitty",
+			processor = "magick_cli",
+			tmux_show_only_in_active_window = true,
+		},
+	},
 	{ "wakatime/vim-wakatime", lazy = false },
 	{
 		"OXY2DEV/markview.nvim",

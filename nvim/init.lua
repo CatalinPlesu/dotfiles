b12f99cf18a8a41556e09b16531dd7c54e74de33
Catalin Plesu <catalinhimself@gmail.com>
2026-01-17 17:34:44 +0200

    feat: easy dotnet some configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -330,6 +330,7 @@ require("lazy").setup({
 			max_width = function()
 				return math.floor(vim.o.columns * 0.75)
 			end,
+			background_colour = "#000000",
 		},
 		config = function(_, opts)
 			require("notify").setup(opts)
@@ -626,9 +627,65 @@ require("lazy").setup({
 
 	{
 		"GustavEikaas/easy-dotnet.nvim",
+		-- 'nvim-telescope/telescope.nvim' or 'ibhagwan/fzf-lua' or 'folke/snacks.nvim'
+		-- are highly recommended for a better experience
 		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
 		config = function()
-			require("easy-dotnet").setup()
+			local dotnet = require("easy-dotnet")
+			-- Options are not required
+			dotnet.setup({
+				lsp = {
+					enabled = false,
+				},
+				test_runner = {
+					viewmode = "buf",
+					enable_buffer_test_execution = false, --Experimental, run tests directly from buffer
+					noBuild = false,
+					mappings = {
+						run_test_from_buffer = { lhs = "<leader>r", desc = "run test from buffer" },
+						run_all_tests_from_buffer = { lhs = "<leader>t", desc = "run all tests from buffer" },
+						peek_stack_trace_from_buffer = { lhs = "<leader>p", desc = "peek stack trace from buffer" },
+						filter_failed_tests = { lhs = "<leader>fe", desc = "filter failed tests" },
+						debug_test = { lhs = "<leader>d", desc = "debug test" },
+						go_to_file = { lhs = "g", desc = "go to file" },
+						run_all = { lhs = "<leader>R", desc = "run all tests" },
+						run = { lhs = "<leader>r", desc = "run test" },
+						peek_stacktrace = { lhs = "<leader>p", desc = "peek stacktrace of failed test" },
+						expand = { lhs = "o", desc = "expand" },
+						expand_node = { lhs = "E", desc = "expand node" },
+						expand_all = { lhs = "-", desc = "expand all" },
+						collapse_all = { lhs = "W", desc = "collapse all" },
+						close = { lhs = "q", desc = "close testrunner" },
+						refresh_testrunner = { lhs = "<C-r>", desc = "refresh testrunner" },
+					},
+					--- Optional table of extra args e.g "--blame crash"
+					additional_args = {},
+				},
+				new = {
+					project = {
+						prefix = "sln", -- "sln" | "none"
+					},
+				},
+				server = {
+					---@type nil | "Off" | "Critical" | "Error" | "Warning" | "Information" | "Verbose" | "All"
+					log_level = nil,
+				},
+				-- choose which picker to use with the plugin
+				-- possible values are "telescope" | "fzf" | "snacks" | "basic"
+				-- if no picker is specified, the plugin will determine
+				-- the available one automatically with this priority:
+				-- telescope -> fzf -> snacks ->  basic
+				picker = "fzf",
+				background_scanning = true,
+			})
+
+			-- Example command
+			vim.api.nvim_create_user_command("Secrets", function()
+				dotnet.secrets()
+			end, {})
+
+			-- Example keybinding
+			vim.keymap.set("n", "<leader>D", "<cmd>Dotnet<CR>", { desc = "Run Dotnet FZF" })
 		end,
 	},
 

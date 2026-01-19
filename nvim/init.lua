2fa351436106f27b3c7f8044e8db5ac092d0f802
Catalin Plesu <catalin.plesu@jivygroup.com>
2026-01-19 12:54:24 +0200

    mini.files config not to have statusline in buffer

--- nvim/init.lua
+++ nvim/init.lua
@@ -347,7 +347,12 @@ require("lazy").setup({
 		config = function()
 			require("mini.ai").setup({ n_lines = 500 })
 			require("mini.surround").setup()
-			require("mini.files").setup()
+			require("mini.files").setup({
+				window = {},
+				mappings = {},
+				options = {},
+				content = {},
+			})
 			require("mini.comment").setup()
 			require("mini.pairs").setup()
 			require("mini.bufremove").setup()

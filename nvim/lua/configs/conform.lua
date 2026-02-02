0d108c6d9e91278b63eff8f216048d0b32d320d3
Catalin Plesu <catalinhimself@gmail.com>
2026-02-02 19:14:46 +0200

    fix: format on save

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -27,11 +27,11 @@ local opts = {
       stdin = true,
     },
   },
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 require("conform").setup(opts)

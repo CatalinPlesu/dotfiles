8e0166db3232dad3cfa18b546c56762f29a218a3
Catalin Plesu <catalin.plesu@jivygroup.com>
2026-02-11 11:20:20 +0200

    feat: add prune local dead branches

--- git/git-setup.sh
+++ git/git-setup.sh
@@ -3,3 +3,5 @@
 git config --global alias.nuke '!git reset --hard && git clean -fd'
 
 git config --global alias.nuke-all '!git reset --hard && git clean -idx'
+
+git config --global alias.prune-local "!git fetch --prune && git branch -vv | grep ': gone]' | grep -v '*' | awk '{print \$1}' | xargs -r git branch -d"
